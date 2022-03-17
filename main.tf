997096e0510b323cbae1d858b2b4381c888cfae4
ebibm2021 <101542772+ebibm2021@users.noreply.github.com>
2022-03-17 15:18:13 +0530

    Asset code remove: main.tf

--- main.tf
+++ main.tf
@@ -154,26 +154,7 @@ resource "aws_iam_role" "EKSClusterRoleR" {
   })
 }
 
-resource "aws_iam_role" "NodeGroupRole" {
-  name = "EKSNodeGroupRole"
 
-  tags = {
-    resource_identifier_for_turbo_firefly = "turbo_firefly_aws_iam_role_group_role"
-  }
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
