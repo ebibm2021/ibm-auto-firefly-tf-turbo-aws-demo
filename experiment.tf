e997a78e04771943b694447b388656a51c027135
Sujoy Sett <sujosett@in.ibm.com>
2022-03-17 18:36:45 +0530

    yet_another_dummy_vpc
    yet_another_dummy_vpc

--- experiment.tf
+++ experiment.tf
@@ -9,6 +9,26 @@ resource "aws_vpc" "a_dummy_vpc" {
   }
 }
 
+resource "aws_vpc" "another_dummy_vpc" {
+  
+  cidr_block = "10.0.0.0/24"
+  
+  tags = {
+    resource_identifier_for_turbo_firefly = "turbo_firefly_another_dummy_vpc"
+    Name = "another_dummy_vpc"
+  }
+}
+
+resource "aws_vpc" "yet_another_dummy_vpc" {
+  
+  cidr_block = "10.0.0.0/24"
+  
+  tags = {
+    resource_identifier_for_turbo_firefly = "turbo_firefly_yet_another_dummy_vpc"
+    Name = "yet_another_dummy_vpc"
+  }
+}
+
 resource "aws_internet_gateway" "extra_gateway" {
 
   tags = {
