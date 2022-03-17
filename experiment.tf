844de099d84e3911b37e0a7630b8c2ac66a7dd3d
Sujoy Sett <sujosett@in.ibm.com>
2022-03-17 18:32:28 +0530

    cidr block in dummy vpc

--- experiment.tf
+++ experiment.tf
@@ -1,6 +1,8 @@
 
 resource "aws_vpc" "a_dummy_vpc" {
-
+  
+  cidr_block = "10.0.0.0/24"
+  
   tags = {
     resource_identifier_for_turbo_firefly = "turbo_firefly_a_dummy_vpc"
     Name = "a_dummy_vpc"
