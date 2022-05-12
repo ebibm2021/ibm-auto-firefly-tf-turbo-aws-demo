94010dbf1f3f2937a5019e54511e9f7d27f92685
ebibm2021 <101542772+ebibm2021@users.noreply.github.com>
2022-05-12 15:20:59 +0530

    Drift fix: experiment.tf

--- experiment.tf
+++ experiment.tf
@@ -5,7 +5,7 @@ resource "aws_vpc" "a_dummy_vpc" {
   
   tags = {
     resource_identifier_for_turbo_firefly = "turbo_firefly_a_dummy_vpc"
-    Name = "a_dummy_vpc_adasdasdasdasd_YOGHC2TZM1O0M2KUFDL3"
+    Name = "a_dummy_vpc_8E2W5AWZAK8FAY7R6ZNV"
   }
 }
 
